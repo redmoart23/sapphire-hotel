5a44f428bc4957cf06bcfc70bc9be9624725e241
redmo academy <108553638+redmoart23@users.noreply.github.com>
2025-04-05 21:17:23 -0500

    ✨ feat: add Docker support with Dockerfile and docker-compose configuration (#9)

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,29 +4,21 @@ import * as joi from 'joi';
 interface EnvVars {
   NODE_ENV: string;
   PORT: number;
-  POSTGRES_DB_USER: string;
-  POSTGRES_DB_PASSWORD: string;
-  POSTGRES_DB_NAME: string;
-  POSTGRES_DB_HOST: string;
-  POSTGRES_DB_PORT: number;
+  DATABASE_URL: string;
 }
 
 const envsSchema = joi
   .object({
     NODE_ENV: joi.string().valid('dev', 'prod').required(),
     PORT: joi.number().required(),
-    POSTGRES_DB_USER: joi.string().required(),
-    POSTGRES_DB_PASSWORD: joi.string().required(),
-    POSTGRES_DB_NAME: joi.string().required(),
-    POSTGRES_DB_HOST: joi.string().required(),
-    POSTGRES_DB_PORT: joi.number().required(),
+    DATABASE_URL: joi.string().required(),
   })
   .unknown(true);
 
 const { error, value } = envsSchema.validate(process.env);
 
 if (error) {
-  throw new Error(`Config validation error: \${error.message}`);
+  throw new Error(`Config validation error: ${error.message}`);
 }
 
 const envVars: EnvVars = value;
@@ -34,9 +26,5 @@ const envVars: EnvVars = value;
 export const envs = {
   nodeEnv: envVars.NODE_ENV,
   port: envVars.PORT,
-  dbUser: envVars.POSTGRES_DB_USER,
-  dbPassword: envVars.POSTGRES_DB_PASSWORD,
-  dbName: envVars.POSTGRES_DB_NAME,
-  dbHost: envVars.POSTGRES_DB_HOST,
-  dbPort: envVars.POSTGRES_DB_PORT,
+  databaseUrl: envVars.DATABASE_URL,
 };
